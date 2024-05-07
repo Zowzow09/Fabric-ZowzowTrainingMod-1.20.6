362c8177e097ec05701b22183a39d80582ba39a6
Enzo Poilane <enzopoilane@gmail.com>
2024-05-07 18:06:20 +0200

    test

---
+++ src/main/java/net/zowzow/trainingmod/TrainingModClient.java
@@ -0,0 +1,10 @@
+package net.zowzow.trainingmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrainingModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
